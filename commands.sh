67918899f20255814dda30b6b2502c50946a5cd4
Sam <sam.a863.nz@gmail.com>
2023-06-19 11:14:09 +1200

    commads.sh file modified

--- commands.sh
+++ commands.sh
@@ -1,3 +1,11 @@
 # ssh into kali linux AWS ec2 server
 ssh -i "kali-key-pair.pem" kali@ec2-34-203-240-87.compute-1.amazonaws.com
 
+# adduser
+sudo adduser sam
+## enter details (optional)
+
+# user details 
+sudo cat /etc/passwd
+#password
+sudo cat /etc/shadow
